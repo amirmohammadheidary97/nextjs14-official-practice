d7f8d5e1a2877a70f3380c4627d8488c4aba9fea
unknown <00000000000>
2024-03-08 19:04:32 +0330

    debug

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,4 +3,4 @@ import { Lusitana  } from 'next/font/google';
 import { Inter  } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const luis = Lusitana({ subsets: ['latin'],weight:['400','700'] });
+export const lusitana = Lusitana({ subsets: ['latin'],weight:['400','700'] });
